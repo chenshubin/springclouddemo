65c1d1d018b11ff081a96acda97a6d314bb104e8
09067436-陈树斌 <09067436@hucais.com>
2020-11-30 11:28:59 +0800

    springcloud 提交到github

---
+++ demo-client/src/main/java/com/murphy/springclouddemo/democlient/ServiceHi.java
@@ -0,0 +1,25 @@
+package com.murphy.springclouddemo.democlient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/serviceHi")
+public class ServiceHi {
+
+
+    @Value("${server.port}")
+    String port;
+
+
+
+    @RequestMapping("/sayHi")
+    public String sayHi(@RequestParam String name){
+        return "hi " + name + ",i am from port:" + port;
+
+    }
+
+}
