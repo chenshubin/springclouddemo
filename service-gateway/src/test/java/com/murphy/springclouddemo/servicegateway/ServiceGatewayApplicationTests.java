0209a75c77825b365b00e4e434868b45b9571b13
09067436-陈树斌 <09067436@hucais.com>
2020-12-01 21:24:03 +0800

    springcloud 提交到github

---
+++ service-gateway/src/test/java/com/murphy/springclouddemo/servicegateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murphy.springclouddemo.servicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
